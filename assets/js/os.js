8c5853c68bd8dddec80040a880826d115c6fa9af
Darren Wood <darren@thefold.co.nz>
2019-01-27 14:45:44 +1300

    more

--- assets/js/os.js
+++ assets/js/os.js
@@ -1,18 +1,31 @@
-var request = new XMLHttpRequest();
+const request = new XMLHttpRequest();
+const bg = document.getElementById('bg');
+const os = document.getElementById('os');
 
-request.open('GET', 'http://brianeno.needsyourhelp.org/draw', true);
+request.open('GET', 'https://cors.io/?http://brianeno.needsyourhelp.org/draw', true);
 request.onload = function () {
 
 	// Begin accessing JSON data here
 	var data = JSON.parse(this.response);
+	var hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e"];
 
-	if (request.status >= 200 && request.status < 400) {
-		data.forEach(os => {
-			console.log(os.strategy);
-		});
-	} else {
-		console.log('error');
+	function populate(a) {
+		for (var i = 0; i < 6; i++) {
+			var x = Math.round(Math.random() * 14);
+			var y = hexValues[x];
+			a += y;
+		}
+		return a;
 	}
+
+	var newColor1 = populate('#');
+	var newColor2 = populate('#');
+	var angle = Math.round(Math.random() * 360);
+
+	var gradient = "linear-gradient(" + angle + "deg, " + newColor1 + ", " + newColor2 + ")";
+
+	bg.style.background = gradient;
+	os.innerHTML = data.strategy;
 }
 
 request.send();
