f8413c60e733d167015505d295f04bd2d126cd43
Darren Wood <darren@thefold.co.nz>
2019-01-27 15:15:56 +1300

    widow prevention (again)

--- assets/js/os.js
+++ assets/js/os.js
@@ -25,7 +25,9 @@ request.onload = function () {
 	var gradient = "linear-gradient(" + angle + "deg, " + newColor1 + ", " + newColor2 + ")";
 
 	bg.style.background = gradient;
-	os.innerHTML = data.strategy;
+	os.innerHTML = data.strategy.replace(/\s([^\s<]+)\s*$/, \'&nbsp\;$1');
+
+;
 }
 
 request.send();
